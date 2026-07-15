6b6bfecfd18e4c0d00fb0f986b1a54fee36e36a1
marufuu <almaruf58@gmail.com>
2026-07-15 22:35:33 +0600

    Initial Commit

---
+++ NotificationApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotificationApp
+//
+//  Created by Maruf on 15/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotificationApp/NotificationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationAppApp.swift
+//  NotificationApp
+//
+//  Created by Maruf on 15/7/26.
+//
+
+import SwiftUI
+
+@main
+struct NotificationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
